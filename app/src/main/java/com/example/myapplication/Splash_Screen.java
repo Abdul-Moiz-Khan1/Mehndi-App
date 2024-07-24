530b9f520d10c0b7e30cb9ab4a2a5cf8ab5091da
Abdul-Moiz-Khan1 <moizz1326@gmail.com>
2024-07-24 19:01:44 +0500

    splash done , flip!!!!

--- app/src/main/java/com/example/myapplication/Splash_Screen.java
+++ app/src/main/java/com/example/myapplication/Splash_Screen.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,7 +17,16 @@ public class Splash_Screen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
 
-
+        binding.lottie.setAnimation(R.raw.mehndi2);
+        binding.lottie.playAnimation();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash_Screen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
 
     }
 }
