12d67f51d8a7df465d07515682d453fda6a77ef1
Abdul-Moiz-Khan1 <moizz1326@gmail.com>
2024-09-09 19:50:41 +0500

    finalizing

--- app/src/main/java/com/example/myapplication/Adapters/CustomAdapter.java
+++ app/src/main/java/com/example/myapplication/Adapters/CustomAdapter.java
@@ -1,30 +1,17 @@
 package com.example.myapplication.Adapters;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.myapplication.Mehndi;
 import com.example.myapplication.R;
 
-import java.util.List;
-
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-import com.example.myapplication.Adapters.ItemModel;
-import com.example.myapplication.R;
 import java.util.List;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
@@ -38,7 +25,6 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
         this.mData = data;
         this.mListener = listener;
     }
-
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
